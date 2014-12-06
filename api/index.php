6fd3b37e9c46863be63094d2b7f8fcb7dc57aafb
Andrei Laza <andreilaza@Andreis-MacBook-Pro.local>
2014-12-06 13:33:41 +0200

    Added CRUD operations to the database library

--- api/index.php
+++ api/index.php
@@ -1,8 +1,61 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+ 
 require 'vendor/autoload.php';
 
-$app = new \Slim\Slim();
+require 'libraries/database.php';
 
+$app = new \Slim\Slim(array(
+		'debug' => true,
+		'mode'  => 'development'
+	)
+);
+
+$db = new DB();
+
+$app->get('/users', function () use ($app, $db) {
+	
+	$result = $db->getAll('users');
+	
+	$app->response->setBody(json_encode($result));
+
+});
+
+$app->get('/users/:id', function ($id) use ($app, $db) {
+	
+	$result = $db->getById('users', $id);
+	
+	$app->response->setBody(json_encode($result));
+
+});
+
+$app->post('/users', function() use ($app, $db) {	
+	$requestBody['fields'] = json_decode($app->request->getBody(), TRUE);
+	$requestBody['table'] = 'users';
+	
+	$result = $db->create($requestBody);
+
+	$app->response->setBody(json_encode($result));
+
+});
+
+$app->put('/users/:id', function ($id) use ($app, $db) {
+	$requestBody['fields'] = json_decode($app->request->getBody(), TRUE);
+	$requestBody['table'] = 'users';
+
+	$result = $db->update($requestBody);
+
+	$app->response->setBody(json_encode($result), FALSE);
+});
+
+$app->delete('/users/:id', function ($id) use ($app, $db) {
+	$requestBody['fields'] = json_decode($app->request->getBody(), TRUE);
+	$requestBody['table'] = 'users';
+
+	$result = $db->remove($requestBody);
+
+	$app->response->setBody(json_encode($result), FALSE);
+});
+
+$app->run();
---
+++ api/libraries/database.php
@@ -0,0 +1,117 @@
+<?php
+
+class DB {
+
+	private $dbHandler;
+	private $query; 
+
+	// Database config items
+	const DBUSER = 'root';
+	const DBPASS = 'treehuggers';
+	const DBNAME = 'treehuggers';
+	const DBHOST = 'localhost';
+
+	// Construct 
+	public function __construct() {
+		try {
+			$this->dbHandler = new PDO('mysql:host=' . DB::DBHOST . ';dbname=' . DB::DBNAME, DB::DBUSER, DB::DBPASS);
+		} catch (PDOException $e) {
+    	print "Error!: " . $e->getMessage() . "<br/>";
+    	die();
+		}
+	}
+
+	// get all items from table method
+	public function getAll($table = '') {
+		$this->query = "SELECT * FROM " . $table;
+
+		return $this->dbHandler->query($this->query)->fetchAll();
+
+	}
+
+	// get all items from table method
+	public function getById($table = '', $id = 0) {
+		$this->query = "SELECT * FROM `" . $table . "` WHERE `id` = " . $id;
+		
+		return $this->dbHandler->query($this->query)->fetch(PDO::FETCH_ASSOC);
+		
+	}
+
+	// insert method
+	public function create($params = array()) {
+		extract($params);
+
+		$keys = array_keys($fields);
+
+		$this->query = 'INSERT INTO `' . $table . '` '
+     . '(' . implode(', ', $keys) . ') '
+     . "VALUES ('".implode("', '", $fields)."')";    
+
+    $result = $this->dbHandler->query($this->query);
+
+    // throw error
+    if (!$result) {
+    	$error = $this->dbHandler->errorInfo();
+
+    	return array(
+    		'errorMessage' => $error[2],
+    		'status'       => 666
+  		);
+    }
+
+    return $fields;
+	}
+
+
+	// update method
+	public function update($params = array()) {
+		extract($params);
+
+		$this->query = 'UPDATE `' . $table . '` SET ';
+
+		foreach ($fields as $key => $field) {
+			$this->query .= '' . $key . ' = "' . $field .'", ';			
+		}
+
+		$this->query = substr($this->query, 0, -2);
+
+		$this->query .= " WHERE `id` = " . $fields['id'];
+		
+		$result = $this->dbHandler->query($this->query);
+
+		// throw error
+    if (!$result) {
+    	$error = $this->dbHandler->errorInfo();
+
+    	return array(
+    		'errorMessage' => $error[2],
+    		'status'       => 666
+  		);
+    }    
+
+    return $fields; 
+	}
+
+	// delete method
+	public function remove($params = array()) {
+		extract($params); 
+
+		$this->query = 'DELETE FROM `' . $table . '` WHERE `id` = ' . $fields['id'];
+
+		$result = $this->dbHandler->query($this->query);
+
+		// throw error
+    if (!$result) {
+    	$error = $this->dbHandler->errorInfo();
+
+    	return array(
+    		'errorMessage' => $error[2],
+    		'status'       => 666
+  		);
+    }    
+
+    return array(
+    	'status' =>'200'
+    );
+  }
+}
