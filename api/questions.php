0369c8b747902fa7dae41ff3139284dd744b95cc
Andrei Laza <andreilaza@Andreis-MacBook-Pro.local>
2014-12-06 20:24:54 +0200

    Fixed answering

--- api/questions.php
+++ api/questions.php
@@ -24,14 +24,34 @@ $app->get('/questions/:id', function ($id) use ($app, $db) {
 	
 	$result = $db->getById('questions', $id);
 	
+	$answers = $db->getAnswersByQuestionId($result['id']);
+
+	foreach ($answers as $answerKey => $answer) {			
+		$answers[$answerKey]['correct'] = (boolean) $answer['correct'];
+	}		 
+
+	$result['answers'] = $answers;
+
 	$app->response->setBody(json_encode($result));
 
 });
 
 // Create
 $app->post('/questions', function() use ($app, $db) {	
-	$requestBody['fields'] = json_decode($app->request->getBody(), TRUE);
-	$requestBody['table'] = 'questions';
+	$payload               = json_decode($app->request->getBody(), TRUE);
+
+	// determine userId
+	$cookieValue = $_COOKIE['TH-Token'];
+
+	$result = $db->getSessionByToken($cookieValue);	
+
+	$requestBody           = array();
+	$requestBody['table']  = 'users_answers';
+	$requestBody['fields'] = array(
+		'question_id' => $payload['questionId'],
+		'answer_id'   => $payload['answerId'],
+		'user_id'     => $result['user_id']
+	);
 	
 	$result = $db->create($requestBody);
 	
