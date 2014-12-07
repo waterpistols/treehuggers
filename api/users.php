cda2cf83bd1233f1dcddbe963d620173d329c546
Andrei Laza <andreilaza@Andreis-MacBook-Pro.local>
2014-12-07 11:20:24 +0200

    Slight refactoring

--- api/users.php
+++ api/users.php
@@ -56,7 +56,35 @@ $app->post('/login', function() use ($app, $db) {
 		if ($existing) {
 			$requestBody['fields']['first_login'] = 0;
 			$requestBody['fields']['id'] = $existing['id'];
+
+			$result = $db->getById('users', $requestBody['fields']['id']);
 			
+			$session = $db->getSessionByUserId($result['id']);
+
+			// Create a token for the session
+			$salt = microtime();
+			$token = crypt($requestBody['fields']['api_id'] + microtime(), $salt);
+
+			$now = date("Y-m-d H:i:s");
+			$expires = date("Y-m-d H:i:s", strtotime($now) + 3600);
+
+			$params['table'] = 'sessions';
+			$params['fields'] = array(
+				'token'     => $token,
+				'user_id'   => $result['id'],
+				'last_used' => date("Y-m-d H:i:s"),
+				'expires'   => $expires
+			);
+			if ($session) {						
+				$params['fields']['id'] = $session['id'];					
+
+				$db->update($params);
+			}	else {			
+				$db->create($params);
+			}
+
+			$result['token'] = $token;
+			setcookie('TH-Token', $token);
 			$db->update($requestBody);
 			$app->response->setBody(json_encode($existing));
 		} else {
@@ -65,100 +93,62 @@ $app->post('/login', function() use ($app, $db) {
 			$result = $db->create($requestBody);
 
 			// Assign user to island
-			$island = $db->getAssignableIsland();
+			$island = $db->getAssignableIsland();			
 
-			if (!array_key_exists('errorMessage', $result)) {
+			// No island created yet OR no free island
+			if (!$island) {
 
-				// No island created yet OR no free island
-				if (!$island) {
-
-					$params['fields'] = array(
-						'name'    => 'Serenity',
-						'players' => 1
-					);
-					$params['table'] = 'islands';
-
-					$createdIsland = $db->create($params);
-
-					$params['fields'] = array(
-						'user_id' => $result['id'],
-						'island_id' => $createdIsland['id']
-					);
-					$params['table'] = 'islands_users';
-
-					$db->create($params);										
-
-				} else {
-					// There is a free island. Assign the new user to it
-					$params['fields'] = array(
-						'user_id' => $result['id'],
-						'island_id' => $island['id']
-					);
-					$params['table'] = 'islands_users';
-
-					$db->create($params);
-
-					$island['players']++;
-					
-					$params['fields'] = $island;
-					$params['table'] = 'islands';
-					
-					$db->update($params);			
-				}
-
-				// Create a token for the session
-				$salt = microtime();
-				$token = crypt($requestBody['fields']['api_id'] + microtime(), $salt);
-
-				$now = date("Y-m-d H:i:s");
-				$expires = date("Y-m-d H:i:s", strtotime($now) + 3600);
-
-
-				$params['table'] = 'sessions';
 				$params['fields'] = array(
-					'token'     => $token,
-					'user_id'   => $result['id'],
-					'last_used' => date("Y-m-d H:i:s"),
-					'expires'   => $expires
+					'name'    => 'Serenity',
+					'players' => 1
 				);
+				$params['table'] = 'islands';
+
+				$createdIsland = $db->create($params);
+
+				$params['fields'] = array(
+					'user_id' => $result['id'],
+					'island_id' => $createdIsland['id']
+				);
+				$params['table'] = 'islands_users';
+
+				$db->create($params);										
+
+			} else {
+				// There is a free island. Assign the new user to it
+				$params['fields'] = array(
+					'user_id' => $result['id'],
+					'island_id' => $island['id']
+				);
+				$params['table'] = 'islands_users';
 
 				$db->create($params);
-			} else {		
-				$result = $db->getById('users', $requestBody['fields']['id']);
-				$session = $db->getSessionByUserId($result['id']);
 
-				// Create a token for the session
-				$salt = microtime();
-				$token = crypt($requestBody['fields']['api_id'] + microtime(), $salt);
-
-				$now = date("Y-m-d H:i:s");
-				$expires = date("Y-m-d H:i:s", strtotime($now) + 3600);
-
-				$params['table'] = 'sessions';
-				if ($session) {
-							
-					$params['fields'] = array(
-						'id'        => $session['id'],
-						'token'     => $token,
-						'user_id'   => $result['id'],
-						'last_used' => date("Y-m-d H:i:s"),
-						'expires'   => $expires
-					);
-
-					$db->update($params);
-				}	else {			
-			
-					$params['fields'] = array(
-						'token'     => $token,
-						'user_id'   => $result['id'],
-						'last_used' => date("Y-m-d H:i:s"),
-						'expires'   => $expires
-					);
-
-					$db->create($params);
-				}	
-
+				$island['players']++;
+				
+				$params['fields'] = $island;
+				$params['table'] = 'islands';
+				
+				$db->update($params);			
 			}
+
+			// Create a token for the session
+			$salt = microtime();
+			$token = crypt($requestBody['fields']['api_id'] + microtime(), $salt);
+
+			$now = date("Y-m-d H:i:s");
+			$expires = date("Y-m-d H:i:s", strtotime($now) + 3600);
+
+
+			$params['table'] = 'sessions';
+			$params['fields'] = array(
+				'token'     => $token,
+				'user_id'   => $result['id'],
+				'last_used' => date("Y-m-d H:i:s"),
+				'expires'   => $expires
+			);
+
+			$db->create($params);
 
 			$result['token'] = $token;
 			setcookie('TH-Token', $token);
