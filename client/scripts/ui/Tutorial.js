bc0c97a0322bbfd0bd89894e1dcb733dffc06723
Andrei Laza <andreilaza@Andreis-MacBook-Pro.local>
2014-12-08 01:52:29 +0200

    Updated tutorial text

--- client/scripts/ui/Tutorial.js
+++ client/scripts/ui/Tutorial.js
@@ -44,32 +44,32 @@ TH.Tutorial = (function() {
             first: '-first',
             heading: 'HERE YOU ARE',
             img: '',
-            title: 'Treehuggers explained',
-            content: 'Hello'
+            title: 'Welcome, Treehugger!',
+            content: 'Air pollution is a global health issue. Take the tour to quickly learn how you can do your part in helping to clear our air'
         },
         1: {
             id: 1,
             first: '',
             heading: 'TUTORIAL',
             img: '<img src="assets/images/tut1-1.jpg" alt="tutorial" />',
-            title: 'Your activity zone on the island',
-            content: 'You own a quarter of the island. You and your friends need to get together and save the island. Your quarter is splitted in 5 zones on which you can plant trees.'
+            title: 'Your own quarter of the island',
+            content: 'You and your friends need to work together and clear the air on the island. Your quarter is split in five zones on which you can plant trees.'
         },
         2: {
             id: 2,
             first: '',
             heading: 'TUTORIAL',
             img: '<img src="assets/images/tut2-1.jpg" alt="tutorial" />',
-            title: '2',
-            content: '2'
+            title: 'The helping hands',
+            content: 'Your friends spawn on the other territories. Pay attention, they may need your assistance!'
         },
         3: {
             id: 3,
             first: '',
             heading: 'TUTORIAL',
             img: '<img src="assets/images/tut3-1.jpg" alt="tutorial" />',
-            title: '3',
-            content: '3'
+            title: 'The tools of your trade',
+            content: 'You start the game with four trees. Plant them anywhere in your quarter. Answer questions to unlock more trees, but beware! Asnwering incorrectly makes the trees degrade.'
         }
     };
 
